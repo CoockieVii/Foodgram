e34f534791a3b609fbaa6c28c0d7ade9c817e337
ilmir <valitovilmir29@gmail.com>
2022-08-03 16:39:15 +0300

    Developed API for Tag.

--- backend/foodgram/app_api/urls.py
+++ backend/foodgram/app_api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
+from app_tags.views import TagsViewSet
 from app_users.views import UserViewSet, SubscriptionViewSet
 
 app_name = 'app_api'
 
 router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename='users')
+router_v1.register('tags', TagsViewSet, basename='tags')
 
 auth_urls = [
     path('', include('djoser.urls')),
--- backend/foodgram/app_tags/views.py
+++ backend/foodgram/app_tags/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from app_tags.models import Tag
+from app_tags.serializers import TagSerializer
+
+
+class TagsViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [AllowAny]
+    pagination_class = None
