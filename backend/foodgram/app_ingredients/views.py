05d73571fbfd6141e9c2ef1b9ea60c3356f6a8d8
ilmir <valitovilmir29@gmail.com>
2022-08-06 12:28:52 +0300

    Fix app ingradients

--- backend/foodgram/app_ingredients/views.py
+++ backend/foodgram/app_ingredients/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from app_ingredients.models import Ingredient
+from app_ingredients.serializers import IngredientSerializer
+
+
+class IngredientViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    pagination_class = None
+    permission_classes = [AllowAny]
