7bf5025d500c0f03c0f25ee2f71c380c76ac02cd
ilmir <valitovilmir29@gmail.com>
2022-08-06 12:25:21 +0300

    Fix app core

--- backend/foodgram/app_core/mixins.py
+++ backend/foodgram/app_core/mixins.py
@@ -1,29 +1,66 @@
+from app_recipes.models import (ShoppingCart, RecipeTag, Favorite, Recipe,
+                                RecipeIngredientRelations)
 from app_users.models import Subscription
+from app_recipes import serializers
 
 
-class AttributesForUser:
-    def get_is_subscribed(self, obj):
-        """Получение параметра is_subscribed."""
-        user = self.context.get('request').user
-        if Subscription.objects.filter(
-                user=user,
-                author=obj).exists():
-            return True
-        return False
-
-
-class AttributesForSubscription(AttributesForUser):
+class GetRecipesCount:
     def get_recipes_count(self, obj):
-        """Получение параметра recipes_count."""
         return Recipe.objects.filter(author__id=obj.id).count()
 
+
+class GetRecipe:
     def get_recipes(self, obj):
-        """Получение параметра recipes_limit."""
         request = self.context.get('request')
-        if request.GET.get('recipes_limit'):
-            recipes_limit = int(request.GET.get('recipes_limit'))
-            queryset = Recipe.objects.filter(author__id=obj.id).order_by('id')[
-                       :recipes_limit]
+        if request.GET.get('recipe_limit'):
+            recipe_limit = int(request.GET.get('recipe_limit'))
+            queryset = Recipe.objects.filter(
+                author=obj)[:recipe_limit]
         else:
-            queryset = Recipe.objects.filter(author__id=obj.id).order_by('id')
-        return RecipeMinifieldSerializer(queryset, many=True).data
+            queryset = Recipe.objects.filter(
+                author=obj)
+        serializer = serializers.SimpleRecipeSerializer(
+            queryset, read_only=True, many=True)
+        return serializer.data
+
+
+class GetSubscribed:
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        return user.is_authenticated and Subscription.objects.filter(
+            author=obj, user=user).exists()
+
+
+class GetFavorites:
+    def get_is_favorited(self, obj):
+        user_id = self.context.get('request').user.id
+        return Favorite.objects.filter(user=user_id, recipe=obj.id).exists()
+
+
+class GetIngradients:
+    def get_ingradients(self, obj):
+        return RecipeIngredientRelations.objects.filter(recipe=obj)
+
+
+class GetTags:
+    def get_tags(self, obj):
+        return RecipeTag.objects.filter(recipe=obj)
+
+
+class GetShoppingCart:
+    def get_is_in_shopping_cart(self, obj):
+        user_id = self.context.get('request').user.id
+        return ShoppingCart.objects.filter(
+            user=user_id, recipe=obj.id).exists()
+
+
+class AttributesForRecipe(GetFavorites, GetShoppingCart):
+    pass
+
+
+class AttributesForUser(GetSubscribed):
+    pass
+
+
+class AttributesForSubscription(GetRecipe, GetSubscribed, GetRecipesCount):
+    pass
--- backend/foodgram/app_core/validaters.py
+++ backend/foodgram/app_core/validaters.py
@@ -1,13 +1,13 @@
 import re
-
 from rest_framework import serializers, status
+from rest_framework.response import Response
 
+from app_tags.models import Tag
 from app_users.models import Subscription
 
 
 class ValidateUser(object):
     def validate_username(self, username):
-        """Валидация username"""
         text = 'Недопустимый username: '
         if username == 'me':
             raise serializers.ValidationError(text + username)
@@ -28,8 +28,19 @@ def validate_subscription(author, user):
 
 
 class ValidateTags(object):
-    def validate_tags(self, tags):
+    def validate_slug(self, slug):
         text = ' -содержит недопустимые символы.'
-        if re.match(r'^[-a-zA-Z0-9_]+$', tags) is None:
-            raise serializers.ValidationError(tags + text)
+        if re.match(r'^[-a-zA-Z0-9_]+$', slug) is None:
+            raise serializers.ValidationError(slug + text)
 
+
+def validate_tags(data):
+    """Валидация тэгов: отсутствие в request, отсутствие в БД."""
+    if not data:
+        raise Response({'tags': ['Обязательное поле.']})
+    if len(data) < 1:
+        raise Response({'tags': ['Хотя бы один тэг должен быть указан.']})
+    for tag in data:
+        if not Tag.objects.filter(id=tag).exists():
+            raise Response({'tags': ['Тэг отсутствует в БД.']})
+    return data
