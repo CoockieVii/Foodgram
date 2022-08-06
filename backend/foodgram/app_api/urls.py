965a1dbc3968e371f647c4fbeb8667c4f668c716
ilmir <valitovilmir29@gmail.com>
2022-08-06 12:26:14 +0300

    Fix app api

--- backend/foodgram/app_api/urls.py
+++ backend/foodgram/app_api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 
+from app_ingredients.views import IngredientViewSet
+from app_recipes.views import RecipeViewSet
 from app_tags.views import TagsViewSet
 from app_users.views import UserViewSet, SubscriptionViewSet
 
@@ -9,6 +11,8 @@ app_name = 'app_api'
 router_v1 = DefaultRouter()
 router_v1.register('users', UserViewSet, basename='users')
 router_v1.register('tags', TagsViewSet, basename='tags')
+router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 auth_urls = [
     path('', include('djoser.urls')),
@@ -18,12 +22,9 @@ auth_urls = [
 urlpatterns = [
     path('auth/', include(auth_urls)),
     path('users/subscriptions/',
-         SubscriptionViewSet.as_view(
-             {'get': 'list'}
-         ), name='subscriptions'),
+         SubscriptionViewSet.as_view({'get': 'list'}), name='subscriptions'),
     path('users/<users_id>/subscribe/',
-         SubscriptionViewSet.as_view(
-             {'post': 'create', 'delete': 'delete'}
-         ), name='subscribe'),
+         SubscriptionViewSet.as_view({'post': 'create', 'delete': 'delete'}),
+         name='subscribe'),
     path('', include(router_v1.urls)),
 ]
